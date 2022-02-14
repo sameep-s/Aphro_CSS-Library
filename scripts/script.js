0e7f5817bd6eba79164ce8c737a1480ad144dd6f
sameep-s <sameep.sharma98@gmail.com>
2022-02-14 13:14:46 +0530

    rating initial

--- scripts/script.js
+++ scripts/script.js
@@ -17,8 +17,5 @@ btn2.onclick = () => {
     modal.style.display = "none";
 }
 
-window.onclick = (event) => {
-    if (event.target === modal) {
-        modal.style.display = "none";
-    }
-}
+
+
